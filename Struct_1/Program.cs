6d67a9b0b526dee2cf88f16aaba59bdfc5f66653
Ratimir <ra0953029364@gmail.com>
2020-01-20 01:34:33 +0200

    Добавьте файлы проекта.

---
+++ Struct_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Struct_1
+{
+    struct Notebook
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Price { get; set; }
+
+        public Notebook(string Model, string Manufacturer, int Price)
+        {
+            this.Model = Model;
+            this.Manufacturer = Manufacturer;
+            this.Price = Price;
+        }
+
+        public override string ToString()
+        {
+            return $"Model = {Model};\nManufacturer = {Manufacturer};\nPrice = {Price}.\n";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("Aspire", "Acer", 10000);
+
+            Console.WriteLine(notebook.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
